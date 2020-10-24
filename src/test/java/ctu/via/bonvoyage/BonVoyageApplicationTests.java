7c32159e4cde9f29666616709cd9e38eccd7bbbd
Tatiana Kulikova <kuliktat@fel.cvut.cz>
2020-10-24 21:44:58 +0200

    Added simple skeleton

---
+++ src/test/java/ctu/via/bonvoyage/BonVoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package ctu.via.bonvoyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonVoyageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
